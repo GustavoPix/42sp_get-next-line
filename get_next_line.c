a8959e8327414f63a845c0b7fcbbaded5fab8e7e
Gustavo Carvalho <gustavo1net@hotmail.com>
2021-09-17 18:52:24 -0300

    test ok

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: glima-de <glima-de@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/05 12:35:40 by glima-de          #+#    #+#             */
-/*   Updated: 2021/09/16 20:56:11 by glima-de         ###   ########.fr       */
+/*   Updated: 2021/09/17 18:43:14 by glima-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,26 @@
 
 static char	*readFile(int *sr, int fd)
 {
-	char	r[BUFFER_SIZE];
+	char	*r;
 	char	*aux;
 
-	if (BUFFER_SIZE <= 0)
+	r = calloc(sizeof(char), BUFFER_SIZE + 1);
+	if (!r)
+	{
+		*sr = -1;
 		return (NULL);
-	*sr = read(fd, r, BUFFER_SIZE);
-	r[*sr] = '\0';
+	}
+	*sr = read(fd, (void *)r, BUFFER_SIZE);
 	if (*sr <= 0)
+	{
+		free(r);
 		return (NULL);
+	}
 	aux = ft_calloc(sizeof(char), *sr + 1);
 	if (!aux)
 		return (NULL);
 	ft_strlcpy(aux, r, *sr + 1);
+	free(r);
 	return (aux);
 }
 
@@ -47,6 +54,7 @@ static int	findReturnChar(char *str)
 	return (-1);
 }
 
+
 char	*get_next_line(int fd)
 {
 	static char	*lastRead;
@@ -54,6 +62,8 @@ char	*get_next_line(int fd)
 	char		*auxSwap;
 	int			nPos;
 
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
 	nPos = 0;
 	if (lastRead)
 	{
@@ -61,9 +71,7 @@ char	*get_next_line(int fd)
 		if (nPos >= 0)
 		{
 			auxR = ft_calloc(sizeof(char), nPos + 2);
-			//printf("auxR -> |%s|\nlastRead -> |%s|\nnPos -> |%d|",auxR, lastRead, nPos);
 			ft_strlcpy(auxR, lastRead, nPos + 2);
-
 			if (ft_strlen(&lastRead[nPos + 1]))
 				auxSwap = ft_calloc(sizeof(char), ft_strlen(&lastRead[nPos + 1]) + 1);
 			else
@@ -74,25 +82,21 @@ char	*get_next_line(int fd)
 			}
 			if (!auxR || !auxSwap)
 				return (NULL);
-			//printf("|c|");
 			ft_strlcpy(auxSwap, &lastRead[nPos + 1], ft_strlen(&lastRead[nPos]));
 			free(lastRead);
 			lastRead = auxSwap;
-			//free(auxSwap);
 			auxSwap = NULL;
 			return (auxR);
 		}
 		else
 		{
-			auxR = readFile(&nPos,fd);
+			auxR = readFile(&nPos, fd);
 			auxSwap = lastRead;
 			if (nPos <= 0)
 			{
-				//free(auxR);
 				lastRead = NULL;
-				return(auxSwap);
+				return (auxSwap);
 			}
-			//printf("|%s| |%d|\n",auxSwap, nPos);
 			lastRead = ft_strjoin(lastRead, auxR);
 			free(auxSwap);
 			free(auxR);
@@ -103,7 +107,7 @@ char	*get_next_line(int fd)
 	}
 	else
 	{
-		lastRead = readFile(&nPos,fd);
+		lastRead = readFile(&nPos, fd);
 		if (!lastRead)
 			return (NULL);
 		return (get_next_line(fd));
